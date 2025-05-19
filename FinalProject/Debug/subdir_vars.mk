dddda95f5b9e1799a667158902bc2f7cfaf37212
JohnKim8121 <88129626+JohnKim8121@users.noreply.github.com>
2025-05-18 20:33:02 -0400

    Add files via upload

---
+++ FinalProject/Debug/subdir_vars.mk
@@ -0,0 +1,41 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432p4111.cmd 
+
+C_SRCS += \
+../FinalProjectMain.c \
+../startup_msp432p4111_ccs.c \
+../system_msp432p4111.c 
+
+C_DEPS += \
+./FinalProjectMain.d \
+./startup_msp432p4111_ccs.d \
+./system_msp432p4111.d 
+
+OBJS += \
+./FinalProjectMain.obj \
+./startup_msp432p4111_ccs.obj \
+./system_msp432p4111.obj 
+
+OBJS__QUOTED += \
+"FinalProjectMain.obj" \
+"startup_msp432p4111_ccs.obj" \
+"system_msp432p4111.obj" 
+
+C_DEPS__QUOTED += \
+"FinalProjectMain.d" \
+"startup_msp432p4111_ccs.d" \
+"system_msp432p4111.d" 
+
+C_SRCS__QUOTED += \
+"../FinalProjectMain.c" \
+"../startup_msp432p4111_ccs.c" \
+"../system_msp432p4111.c" 
+
+
